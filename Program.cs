0a8cf4ca4f9e5e4abfed39d69c3e899935e018b7
aprince98 <122805943+aprince98@users.noreply.github.com>
2023-12-16 14:34:19 +0000

    done

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1;
+        int num2;
+
+        int result;
+
+        string answer;
+
+        Console.WriteLine("Hello, Welcome to your Calculator!");
+        Console.WriteLine("Please enter your first number.");
+
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine ("Please enter your second number.");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("What type of operation would you like to do?");
+        Console.WriteLine("Please enter a for additon, s for subtraction, m for multiplication or any other key for division.");
+
+        answer = Console.ReadLine();
+
+        if(answer == "a") 
+        {
+            result = num1 + num2;
+        }
+        else if (answer == "s")
+        {
+            result = num1 - num2;
+        }
+        else if (answer == "m")
+        {
+            result = num1 * num2;
+        }
+        else
+        {
+            result = num1 / num2;
+        }
+
+        Console.WriteLine( "The result is " + result);
+        Console.WriteLine("Thank You for using the Calculator");
+    }
+}
